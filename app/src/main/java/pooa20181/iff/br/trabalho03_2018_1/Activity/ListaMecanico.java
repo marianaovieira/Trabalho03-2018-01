c5f2474664af3fbede404b61a1ae472741a56f63
Mariana Vieira <marianaovieira1@gmail.com>
2018-08-01 15:06:00 -0300

    Criação das telas de lista
    Signed-off-by: Mariana Vieira <marianaovieira1@gmail.com>

---
+++ app/src/main/java/pooa20181/iff/br/trabalho03_2018_1/Activity/ListaMecanico.java
@@ -0,0 +1,30 @@
+package pooa20181.iff.br.trabalho03_2018_1.Activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import pooa20181.iff.br.trabalho03_2018_1.R;
+
+public class ListaMecanico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_mecanico);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+}
